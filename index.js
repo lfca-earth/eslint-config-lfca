c32721065da45ce166f0266e8e32ba5a85b574aa
David Ruisinger <david.ruisinger@gmail.com>
2023-01-30 14:05:24 +0100

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'next',
+  ],
+  plugins: ['simple-import-sort', 'sort-destructure-keys'],
+  rules: {
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        typedefs: false,
+      },
+    ],
+    'no-console': [
+      'error',
+      {
+        allow: ['info', 'error'],
+      },
+    ],
+    'react/jsx-sort-props': [
+      'error',
+      {
+        ignoreCase: false,
+      },
+    ],
+    'react/prop-types': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-destructure-keys/sort-destructure-keys': 'error',
+    'sort-keys': [
+      'error',
+      'asc',
+      {
+        caseSensitive: false,
+        minKeys: 2,
+        natural: false,
+      },
+    ],
+    'sort-vars': 'error',
+  },
+}
